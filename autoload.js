dc6801f9dca79efad77144050bd574e3657c1b02
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-12-31 07:53:19 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			cdnPath: "https://cdn.jsdelivr.net/gh/XXKDB/live2d_api/"
+			cdnPath: "http://v6xiao.v6.navy/live2d/api/"
 		});
 	});
 }
